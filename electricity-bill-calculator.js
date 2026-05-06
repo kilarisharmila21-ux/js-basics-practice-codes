601b94d754f7430ffc31408c23c252f9bf4b72a7
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-05-06 23:43:53 +0530

    A electricity bill calculator built using JS.

---
+++ electricity-bill-calculator.js
@@ -0,0 +1,9 @@
+// Electricity bill calculator
+
+let units = 120;
+let costPerUnit = 6;
+
+let totalBill = units * costPerUnit;
+
+console.log("units Used:", units);
+console.log("Total Bill:", totalBill);
