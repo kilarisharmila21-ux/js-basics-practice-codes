159f086d0aa3788dd7a4313292d4862c64364e7a
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-05-06 23:44:37 +0530

    A mobile data usage calculator using JS.

---
+++ mobile-data-usage-calculator.js
@@ -0,0 +1,8 @@
+// Mobile data usage
+
+let dailyDataUsage = 1.5; // GB per day
+let days = 28;
+
+let monthlyDataUsage = dailyDataUsage * days;
+
+console.log("Monthly Data Usage:", monthlyDataUsage + "GB");
