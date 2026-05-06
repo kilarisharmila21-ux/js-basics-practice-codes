fbeb6f73923f9c6c240f954fbc026cb5866a3a27
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-05-06 23:49:55 +0530

    A student marks calculator built using JS.

---
+++ student-marks-calculator.js
@@ -0,0 +1,14 @@
+// Student Marks Calculator
+
+let studentName = "Chinnu";
+
+let telugu = 85;
+let english = 78;
+let maths = 90;
+
+let total = telugu + english + maths;
+let average = total/3;
+
+console.log("Student:", studentName);
+console.log("Total Marks:", total);
+console.log("Average:", average);
